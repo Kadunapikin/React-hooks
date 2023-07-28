a8c32368fd9031cf85de4e2ae71667712e645278
kadunapikin <ibakoshi27@gmail.com>
2023-07-28 14:10:20 +0100

    create a handleChange function allow user input new value

--- react-hooks/src/state-hook/EditProfile.js
+++ react-hooks/src/state-hook/EditProfile.js
@@ -2,30 +2,41 @@ import React, { useState } from "react";
 
 export default function EditProfile() {
     const[profile, setProfile] = useState({});
+
+    const handleChange = ({ target }) => {
+        const {name, value} = target;
+        setProfile((prev) => ({
+            ...prev, [name]: value,
+        }));
+    };
     return (
         <form>
             <input
                 value={profile.firstName || ""}
                 name="firstName"
                 type="text"
-                placeholder="first Name" 
+                placeholder="first Name"
+                onChange={handleChange}
             />
             <input
                 value={profile.lastName || ""}
                 name="lastName"
                 type="text"
-                placeholder="last Name" 
+                placeholder="last Name"
+                onChange={handleChange} 
             />
             <input
                 value={profile.bday || ""}
                 name="bday"
                 type="date"
+                onChange={handleChange}
             />
             <input
                 value={profile.password || ""}
                 name="password"
                 type="password"
                 placeholder="Password" 
+                onChange={handleChange}
             />
         </form>
     )
