6835628e897dd7fdd3288add4cdec1fbdb548cca
kadunapikin <ibakoshi27@gmail.com>
2023-07-28 13:20:47 +0100

    update GroceryCart.js and render on App.js

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import GroceryCart from './state-hook/GroceryCart';
 import PhoneNumber from './state-hook/PhoneNumber';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <div>
         <PhoneNumber />
+        <GroceryCart />
       </div>
     </div>
   );
