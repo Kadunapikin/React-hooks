08462bf077ede157ba617ba652786645ffe8d0a1
kadunapikin <ibakoshi27@gmail.com>
2023-07-28 20:35:55 +0100

    import useEffect from react library, create a setInterval function to take setTime callback function and 1000s as parameters

--- react-hooks/src/state-hook/Timer.js
+++ react-hooks/src/state-hook/Timer.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Timer() {
     const [time, setTime] = useState(0)
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime((prevTime) => prevTime + 1);
+        }, 1000)
+    })
     return (
         <>
             <h1>Time: {time}</h1>
