c2dc2def82d4e3465db6bfd45696998f98ccb29f
kadunapikin <ibakoshi27@gmail.com>
2023-07-28 13:28:16 +0100

    create an EditProfile component, declare current and state setter variable with initial state as an empty object

---
+++ react-hooks/src/state-hook/EditProfile.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export default function EditProfile() {
+    const[profile, setProfile] = useState({});
+    return (
+        <form>
+
+        </form>
+    )
+}
