5eba08cd2ff59dfed459662878087f1698707c9b
kadunapikin <ibakoshi27@gmail.com>
2023-07-28 12:48:22 +0100

    create addItem and removeItem function

--- react-hooks/src/state-hook/GroceryCart.js
+++ react-hooks/src/state-hook/GroceryCart.js
@@ -6,6 +6,18 @@ import { produce, pantryItems } from "./storeItems";
 export default function GroceryCart () {
     const [cart, setCart] = useState({});
 
+    const addItem = (item) => {
+        setCart((prev) => {
+            [item, ...prev];
+        });
+    };
+
+    const removeItem = (targetIndex) => {
+        setCart((prev) => {
+            return prev.filter((item, index) => index !==targetIndex);
+        });
+    };
+
     return (
         <div>
 
