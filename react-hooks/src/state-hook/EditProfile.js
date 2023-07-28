5fd3e9ffffd4cd338719d68f5b8291389fb407ab
kadunapikin <ibakoshi27@gmail.com>
2023-07-28 13:56:08 +0100

    return 4 different input tag in the JSX field with value, type and placeholder

--- react-hooks/src/state-hook/EditProfile.js
+++ react-hooks/src/state-hook/EditProfile.js
@@ -4,7 +4,29 @@ export default function EditProfile() {
     const[profile, setProfile] = useState({});
     return (
         <form>
-
+            <input
+                value={profile.firstName || ""}
+                name="firstName"
+                type="text"
+                placeholder="first Name" 
+            />
+            <input
+                value={profile.lastName || ""}
+                name="lastName"
+                type="text"
+                placeholder="last Name" 
+            />
+            <input
+                value={profile.bday || ""}
+                name="bday"
+                type="date"
+            />
+            <input
+                value={profile.password || ""}
+                name="password"
+                type="password"
+                placeholder="Password" 
+            />
         </form>
     )
 }
