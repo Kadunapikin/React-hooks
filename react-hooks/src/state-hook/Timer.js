ee8ada575931d763c391f7dd7382d5319585201a
kadunapikin <ibakoshi27@gmail.com>
2023-07-28 20:58:54 +0100

    declare a new state variable name and a state setter with initial state of an empty string

--- react-hooks/src/state-hook/Timer.js
+++ react-hooks/src/state-hook/Timer.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 
 export default function Timer() {
-    const [time, setTime] = useState(0)
+    const [time, setTime] = useState(0);
+    const [name, setName] = useState('');
     useEffect(() => {
         const intervalId = setInterval(() => {
             setTime((prevTime) => prevTime + 1);
