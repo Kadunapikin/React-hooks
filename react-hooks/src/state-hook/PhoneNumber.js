4f493ba19937d823e7ba0c13a67a613468cb3bd7
kadunapikin <ibakoshi27@gmail.com>
2023-07-28 10:44:25 +0100

    return a JSX input tag and specify phone as its value

--- react-hooks/src/state-hook/PhoneNumber.js
+++ react-hooks/src/state-hook/PhoneNumber.js
@@ -4,7 +4,9 @@ const validPhoneNumber = /^\d{1,10}$/;
 export default function PhoneNumber() {
     const [phone, setPhone] = useState('');
     return (
-        <>
-        </>
+        <div>
+            <label for='phone-input'>Phone</label>
+            <input id='phone-input' value={phone} />
+        </div>
     )
 }
