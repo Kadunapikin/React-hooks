753924ebaa354460e78c0f2c273034230581a3ff
kadunapikin <ibakoshi27@gmail.com>
2023-07-28 10:58:05 +0100

    create an eventListener to check the validity of input user enters and update the state variable

--- react-hooks/src/state-hook/PhoneNumber.js
+++ react-hooks/src/state-hook/PhoneNumber.js
@@ -3,6 +3,15 @@ const validPhoneNumber = /^\d{1,10}$/;
 
 export default function PhoneNumber() {
     const [phone, setPhone] = useState('');
+
+    const handleChange = ({ target }) => {
+        const newPhone = target.value;
+        const isValid = validPhoneNumber.test(newPhone);
+
+        if (isValid) {
+            setPhone(newPhone)
+        }
+    };
     return (
         <div>
             <label for='phone-input'>Phone</label>
